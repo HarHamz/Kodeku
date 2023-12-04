affc9b1722a91103ed1e567d71c81a5314ef171b
Healm <116085297+Agent-Healm@users.noreply.github.com>
2023-12-04 18:14:58 +0700

    Add files via upload
    overhauled to keep the user logged on

---
+++ kodeku-main-overhaul/contact.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kodeku</title>
+    <link rel="icon" href="img/logo_only.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <!-- <header class="navbar-container">
+        <a class="navbar-brand px-3" href="index.html"><img src="img/logo_sekunder.png" alt="Logo" class="main-logo"></a>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <ul>
+                <li>
+                    <a class="nav-link" href="index.html#pascal">Pascal</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="logsign">
+            <a class="btn btn-primary" href="login.html" role="button">Log In</a>
+            <a class="btn btn-primary" href="signup.html" role="button">Sign Up</a>
+        </div>
+    </header> -->
+    <?php
+        include "header.php";
+    ?>
+
+    <!-- Main -->
+    <main>
+        <!-- Background Article -->
+        <article class="container-fluid top">
+            <div class="background-gradient section-title">
+                <div class="main-text">
+                    <h1>Kontak Kami</h1>
+                </div>
+            </div>
+        </article>
+
+        <!-- Section Bab -->
+        <section class="container-fluid" id="contact">
+            <div class="section-row">
+                <form action="message.php">
+                    <div class="mb-3">
+                      <label for="input_nama" class="form-label">Nama</label>
+                      <input type="text" class="form-control" id="input_nama" placeholder="Nama" name="name" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="input_email" class="form-label">Alamat Email</label>
+                      <input type="email" class="form-control" id="input_email" placeholder="contoh@gmail.com" name="email" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="message" class="form-label">Pesan</label>
+                      <textarea class="form-control" id="input_message" placeholder="Pesan" name="message" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </section>
+    </main>
+
+
+    <!-- Footer -->
+    <!-- <footer>
+        &copy; 2023 - Kodeku
+        <a class="navbar-brand px-3" href="index.html"><img src="img/logo_primer.png" alt="Logo" class="main-logo"></a>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">   
+            <ul>
+                <li><a class="nav-link" href="index.html">Beranda</a></li>
+                <li><a class="nav-link" href="contact.html">Kontak</a></li>
+                <li><a class="nav-link" href="about.html">Tentang</a></li>
+            </ul>
+        </nav>
+    </footer> -->
+    <?php
+        include "footer.php";
+    ?>
+
+    <script src="script.js"></script>
+</body>
+</html>
